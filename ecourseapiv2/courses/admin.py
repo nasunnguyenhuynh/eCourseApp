fb9b1145b5efa033947ba9ffd26f18fe206409c4
Nasun Nguyễn Huỳnh <nguyenhuynhnasun@gmail.com>
2024-03-11 21:34:37 +0700

    create models; add ckeditor, cloudinary; custom admin

---
+++ ecourseapiv2/courses/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from courses.models import Category, Course
+from django.utils.html import mark_safe
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class CourseForm(forms.ModelForm):  # add static be4 url when send it to server
+    description = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class CourseAdmin(admin.ModelAdmin):  # Custom admin view
+    list_display = ['id', 'name', 'created_date', 'updated_date', 'active']
+    search_fields = ['name', 'description']
+    list_filter = ['id', 'name', 'created_date']
+    readonly_fields = ['my_image']
+    form = CourseForm
+
+    def my_image(self, course):
+        if course.image:
+            return mark_safe(f'<img src="{course.image.url}" width="200" />')
+
+    class Media:
+        css = {
+            'all': ('/static/css/style.css',)
+        }
+        # js = ('/static/js/script.js',)
+
+
+admin.site.register(Category)
+admin.site.register(Course, CourseAdmin)
---
+++ ecourseapiv2/courses/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+from cloudinary.models import CloudinaryField
+
+
+class User(AbstractUser):
+    pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class BaseModel(models.Model):  # Create BaseModel
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-id']
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(BaseModel):
+    name = models.CharField(max_length=255)
+    description = RichTextField(null=True)
+    image = CloudinaryField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(BaseModel):
+    subject = models.CharField(max_length=255)
+    content = RichTextField(null=True)
+    image = CloudinaryField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)  # Automatic create middle table
+
+    def __str__(self):
+        return self.subject
+
+
+class Interaction(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Comment(Interaction):
+    comment = models.CharField(max_length=255)
+
+
+class Like(Interaction):
+    class Meta:
+        unique_together = ('user', 'lesson')
